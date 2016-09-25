758dcfc03a1ecfe19f87a341609686e4015872bc
epicodus <epicodus@BOGAs-MacBook-Pro.local>
2016-09-25 01:30:32 -0700

    add border to current selected group in accordian menu

--- web/js/myscripts.js
+++ web/js/myscripts.js
@@ -1,8 +1,8 @@
 $(function(){
     $(".accordian").click(function(){
         $(this).next().toggleClass("show");
+    });
+    $(".infoTop").click(function(){
+        $(this).parent().toggleClass("outline");
     })
-
-
-
-})
+});
