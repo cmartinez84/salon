18516552baa15ce9169b351a80a61f3eaf3479e6
epicodus <epicodus@BOGAs-MacBook-Pro.local>
2016-09-24 21:52:55 -0700

    add background and permanent marker font

--- web/index.php
+++ web/index.php
@@ -1,4 +1,21 @@
 <?php
     $website = require_once __DIR__.'/../app/app.php';
     $website->run();
-?>
+ ?>
+ <!DOCTYPE html>
+ <html>
+   <head>
+      <link href="https://fonts.googleapis.com/css?family=Josefin+Slab|Lobster|Play" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Anton|Open+Sans|Pacifico|Permanent+Marker" rel="stylesheet">
+     <link rel="stylesheet" href="/css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
+     <link rel="stylesheet" href="/css/animate.css" media="screen" title="no title" charset="utf-8">
+     <link rel="stylesheet" href="/css/mystyles.css" media="screen" title="no title" charset="utf-8">
+
+     <script src="/js/jquery-3.1.0.min.js"></script>
+     <script src="/js/myscripts.js"></script>
+     <title>Wheel o' Fortune</title>
+   </head>
+   <body>
+
+   </body>
+ </html>
